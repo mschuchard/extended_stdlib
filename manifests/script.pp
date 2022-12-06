c801e6d18b22dba39bb2377d166bb5b2b4144b0e
Matt Schuchard <grapheneowns@gmail.com>
2022-12-06 16:20:18 -0500

    add stage_dir param

--- manifests/script.pp
+++ manifests/script.pp
@@ -11,6 +11,8 @@
 #   The module containing the script. Defaults to the module where this resource is declared.
 # @param shell_path
 #   The path to the shell interpreter executable used for executing the managed script file.
+# @param stage_dir
+#   The path to the staging directory for managing the script file on the target system prior to execution.
 # @param file_attr
 #   A hash of additional attribute => value pairs to append to the file resource that manages the script.
 # @param exec_attr
@@ -19,10 +21,11 @@ define extended_stdlib::script (
   String $script     = $title,
   String $module     = $caller_module_name,
   String $shell_path = '/bin/sh',
+  String $stage_dir  = '/tmp',
   Hash $file_attr    = {},
   Hash $exec_attr    = {}
 ) {
-  file { "/tmp/${script}":
+  file { "${stage_dir}/${script}":
     ensure => file,
     source => "puppet:///modules/${module}/${script}",
     *      => $file_attr,
@@ -30,7 +33,7 @@ define extended_stdlib::script (
 
   exec { "execute ${script} with ${shell_path}":
     command     => "${shell_path} /tmp/${script}",
-    subscribe   => File["/tmp/${script}"],
+    subscribe   => File["${stage_dir}/${script}"],
     refreshonly => true,
     *           => $exec_attr,
   }
