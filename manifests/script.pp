8ed8d9fc2106c07fadba77a81e87a6fb4b86594c
Matt Schuchard <grapheneowns@gmail.com>
2022-12-06 16:17:38 -0500

    add shell_path param

--- manifests/script.pp
+++ manifests/script.pp
@@ -2,30 +2,34 @@
 #
 # Manages a script's existence in the directory of a system, and then executes the script if the script's attributes changed.
 #
-# @example Manages a script named 'myscript.sh' on the target system and sourced from the containing module, and then executes the script with the shell interpreter.
+# @example Manages a script named 'myscript.sh' on the target system and sourced from the containing module, and then executes the script with the default shell interpreter.
 #   extended_stdlib::script { 'myscript.sh': }
 #
 # @param script
 #   The filename of the script to be managed and executed. Defaults to the title of the resource.
 # @param module
 #   The module containing the script. Defaults to the module where this resource is declared.
+# @param shell_path
+#   The path to the shell interpreter executable used for executing the managed script file.
 # @param file_attr
 #   A hash of additional attribute => value pairs to append to the file resource that manages the script.
 # @param exec_attr
 #   A hash of additional attribute => value pairs to append to the exec resource that execures the script.
 define extended_stdlib::script (
-  String $script  = $title,
-  String $module  = $caller_module_name,
-  Hash $file_attr = {},
-  Hash $exec_attr = {})
-{
+  String $script     = $title,
+  String $module     = $caller_module_name,
+  String $shell_path = '/bin/sh',
+  Hash $file_attr    = {},
+  Hash $exec_attr    = {}
+) {
   file { "/tmp/${script}":
     ensure => file,
     source => "puppet:///modules/${module}/${script}",
     *      => $file_attr,
   }
 
-  exec { "/bin/sh /tmp/${script}":
+  exec { "execute ${script} with ${shell_path}":
+    command     => "${shell_path} /tmp/${script}",
     subscribe   => File["/tmp/${script}"],
     refreshonly => true,
     *           => $exec_attr,
