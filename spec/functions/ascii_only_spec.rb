23aa8bcd9f90bb9db87f4a0bba21b7f10eda11fe
Matt Schuchard <grapheneowns@gmail.com>
2023-01-12 10:26:30 -0500

    add unit test and fix circle dir name

--- spec/functions/ascii_only_spec.rb
+++ spec/functions/ascii_only_spec.rb
@@ -4,5 +4,6 @@ require 'spec_helper'
 
 describe 'extended_stdlib::ascii_only' do
   it { is_expected.to run.with_params('abc').and_return(true) }
+  it { is_expected.to run.with_params('abc', 'UTF-8').and_return(true) }
   it { is_expected.to run.with_params("abc\u{6666}").and_return(false) }
 end
