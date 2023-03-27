1658e70cc9248d0b58966bd26d50dff79835ecb5
Matt Schuchard <grapheneowns@gmail.com>
2023-03-27 14:09:39 -0400

    code cleanup

--- lib/facter/powershell_version.rb
+++ lib/facter/powershell_version.rb
@@ -1,12 +1,10 @@
 Facter.add(:powershell_version) do
   confine osfamily: %r{[wW]indows}
   setcode do
-    begin
-      raw_version = Facter::Core::Execution.execute('powershell -command $PSVersionTable.PSVersion.ToString()')
-      %r{(\d+\.\d+\.\d+)}.match(raw_version)[0]
-    rescue Facter::Core::Execution::ExecutionFailure
-      Facter.warn 'Powershell version could not be determined and will be set to 0.0.0'
-      '0.0.0'
-    end
+    raw_version = Facter::Core::Execution.execute('powershell -command $PSVersionTable.PSVersion.ToString()')
+    %r{(\d+\.\d+\.\d+)}.match(raw_version)[0]
+  rescue Facter::Core::Execution::ExecutionFailure
+    Facter.warn 'Powershell version could not be determined and will be set to 0.0.0'
+    '0.0.0'
   end
 end
--- lib/puppet/functions/extended_stdlib/log.rb
+++ lib/puppet/functions/extended_stdlib/log.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # Returns the logarithm of x. If an additional second argument is given, then it will be the logarithm base. Otherwise it is e (for the natural logarithm).
-Puppet::Functions.create_function(:"extended_stdlib::log") do
+Puppet::Functions.create_function(:'extended_stdlib::log') do
   dispatch :log do
-    param 'Numeric', :x
+    param 'Numeric', :num
     optional_param 'Numeric', :base
     return_type 'Float'
   end
@@ -14,7 +14,7 @@ Puppet::Functions.create_function(:"extended_stdlib::log") do
   #   log(1) => 0.0
   #   log(12, 3) => 2.2618595071429146
   #   log(1000, 10) => 2.9999999999999996
-  def log(x, base = nil)
-    base.nil? ? Math.log(x) : Math.log(x, base)
+  def log(num, base = nil)
+    base.nil? ? Math.log(num) : Math.log(x, base)
   end
 end
