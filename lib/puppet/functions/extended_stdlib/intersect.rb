ac9cb04b7b654de29bb9ae796c3b6eb778da7b5a
Matt Schuchard <grapheneowns@gmail.com>
2024-06-03 10:31:35 -0400

    Array.intersect? removed from ruby 3.0

--- lib/puppet/functions/extended_stdlib/intersect.rb
+++ lib/puppet/functions/extended_stdlib/intersect.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Returns true if the array and other_array have at least one element in common, otherwise returns false. Requires Ruby >= 3.0.
+# Returns true if the array and other_array have at least one element in common, otherwise returns false. Requires Ruby >= 3.1.
 Puppet::Functions.create_function(:'extended_stdlib::intersect') do
   # @param a_array The first array to determine if intersects.
   # @param other_array The second array to determine if intersects.
@@ -15,10 +15,10 @@ Puppet::Functions.create_function(:'extended_stdlib::intersect') do
   end
 
   def intersect(a_array, other_array)
-    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.0')
+    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.1')
       a_array.intersect?(other_array)
     else
-      Puppet.warning('The intersect function is not supported in Ruby prior to version 3.0.')
+      Puppet.warning('The intersect function is not supported in Ruby prior to version 3.1.')
       false
     end
   end
--- spec/functions/intersect_spec.rb
+++ spec/functions/intersect_spec.rb
@@ -3,10 +3,8 @@
 require 'spec_helper'
 
 describe 'extended_stdlib::intersect' do
-  if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.0')
+  if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.1')
     it { is_expected.to run.with_params([1, 2, 3], [3, 4, 5]).and_return(true) }
-  else
-    it { is_expected.to run.with_params([1, 2, 3], [3, 4, 5]).and_return(false) }
+    it { is_expected.to run.with_params([1, 2, 3], [5, 6, 7]).and_return(false) }
   end
-  it { is_expected.to run.with_params([1, 2, 3], [5, 6, 7]).and_return(false) }
 end
