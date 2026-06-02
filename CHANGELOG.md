f1d0dbb6a836386b1054086d6f4ed5144efd5b94
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-06-02 10:51:49 -0400

    safeguard powershell_version regex match

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # 1.3.2 (Next)
 - Return informative string for `subnet` fact value instead of `nil` when determinaion fails.
 - Fail faster for usage errors in `imperative` function.
-
+- Safeguard `powershell_version` determination.
 # 1.3.1
 - Fix docstrings issues.
 - Input parameter validation for `drop`, `center`, `sqrt`, `sample`, `transpose`, and `log` functions.
--- lib/facter/powershell_version.rb
+++ lib/facter/powershell_version.rb
@@ -3,8 +3,16 @@ require 'facter'
 Facter.add(:powershell_version) do
   confine osfamily: %r{[wW]indows}
   setcode do
+    # determine powershell version
     raw_version = Facter::Core::Execution.execute('powershell -command $PSVersionTable.PSVersion.ToString()')
-    %r{(\d+\.\d+\.\d+)}.match(raw_version)[0]
+    version = %r{(\d+\.\d+\.\d+)}.match(raw_version)[0]
+
+    # validate powershell version
+    if version.nil?
+      Facter.warn 'Powershell version could not be determined and will be set to 0.0.0'
+      '0.0.0'
+    end
+    version
   rescue Facter::Core::Execution::ExecutionFailure
     Facter.warn 'Powershell version could not be determined and will be set to 0.0.0'
     '0.0.0'
