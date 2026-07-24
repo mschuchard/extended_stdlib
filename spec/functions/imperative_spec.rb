771b754136ae20b36ec58ec3d4dd5fc6f4e73908
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-07-24 15:46:14 -0400

    fix and improve unit tests for imperative function

--- spec/functions/imperative_spec.rb
+++ spec/functions/imperative_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe 'extended_stdlib::imperative' do
+  let(:pre_condition) do
+    <<~PUPPET
+      user { 'foo': }
+      user { 'bar': }
+    PUPPET
+  end
+
   it { is_expected.to run.with_params([Puppet::Resource.new(:user, 'foo'), Puppet::Resource.new(:user, 'bar')]).and_return([Puppet::Resource.new(:user, 'foo'), Puppet::Resource.new(:user, 'bar')]) }
+  it { is_expected.to run.with_params([Puppet::Resource.new(:user, 'foo'), Puppet::Resource.new(:user, 'baz')]).and_raise_error(Puppet::PreformattedError, %r{The resource User\[baz\] is not declared and thus cannot be used with dependency mapping for imperative application}) }
 end
